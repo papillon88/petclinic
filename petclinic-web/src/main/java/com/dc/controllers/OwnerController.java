ffc3cfbf700e5e6462d76200c6e5404a3a627f90
papillon88 <devopreet.ch@gmail.com>
2019-02-08 18:48:24 -0500

    closes #16

--- petclinic-web/src/main/java/com/dc/controllers/OwnerController.java
+++ petclinic-web/src/main/java/com/dc/controllers/OwnerController.java
@@ -1,14 +1,28 @@
 package com.dc.controllers;
 
+import com.dc.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping(value = {"/owners"})
 @Controller
 public class OwnerController {
 
-    @RequestMapping(value = {"/owners","/owners/index","/owners/index.html"}, method = RequestMethod.GET)
-    public String listOwners(){
-        return "owner/index";
+
+    private final OwnerService ownerService;
+
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping(value = {"","/","/index","/index.html"}, method = RequestMethod.GET)
+    public String listOwners(Model model){
+        model.addAttribute("owners",this.ownerService.findAll());
+        return "owners/index";
     }
 }
